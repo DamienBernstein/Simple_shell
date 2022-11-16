d2ae00fc4d04f1c61906fc2836ae4d8dd119306a
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-17 01:36:55 +0200

    Update newshell.c

--- newshell.c
+++ newshell.c
@@ -149,9 +149,11 @@ int main(int ac, char **av, char **env)
 			while (paths[j] != NULL)
 			{
 				buffer_path = path[j] + '/' + ptr[0];
-				if (access(buffer_path, X_OK)
+				if (access(buffer_path, X_OK) == 0)
+					execve(buffer, ptr, env);
 				++j;
 			}
+			printf("function not found");
 		}
 		free(buffer);
 	
