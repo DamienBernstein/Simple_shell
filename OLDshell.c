311d9d695a885c6db28eabe2c4aaacf814bee2d4
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-11-16 21:30:02 +0200

    Update OLDshell.c

--- OLDshell.c
+++ OLDshell.c
@@ -130,6 +130,7 @@ if (strcmp(str[0], "exit") == 0)
 exit(0);
 free(buffer);
 free(str);
+free(str2);
 return (0);
 }
 
@@ -179,6 +180,7 @@ if (child_pid != 0)
 wait(&status);
 free(buffer);
 free(str);
+free(str2);
 main(ac, av, env);
 }
 }
@@ -204,6 +206,7 @@ if (child_pid != 0)
 wait(&status);
 free(buffer);
 free(str);
+free(str2);
 free(path);
 main(ac, av, env);
 }
