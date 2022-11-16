80c6d0f5cebbced6e3a85fc7a43a828d1af69a9d
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-17 01:32:20 +0200

    Update newshell.c

--- newshell.c
+++ newshell.c
@@ -57,27 +57,49 @@ void break_string(char *str, char *delimeter, char **ptr)
 	
 }
 
+int searchforfunction(char *str, char *directory)
+{
+struct dirent *de;
+DIR *dr = opendir(directory);
+if (dr == NULL)
+{
+printf("Could not open current directory");
+return (0);
+}
+
+while ((de = readdir(dr)) != NULL)
+{
+if (strcmp(de->d_name, str) == 0)
+{
+closedir(dr);
+return (1);
+}
+}
+closedir(dr);
+return (0);
+}
+
 
 
 int main(int ac, char **av, char **env)
 {
-	char *buffer, **ptr, *PATH, **paths;
+	char *buffer, **ptr, *PATH, **paths, buffer_path[1024];
 	
-	int characters;
+	int characters, j, access;
 	unsigned int args, i, count, countpaths;
 	size_t size;
 	pid_t process_id;
 	
 	PATH = getenv("PATH");
 	
-	printf("path %s\n", PATH);
+	
 	
 	countpaths = count_args(PATH, ':');
 	
 	paths = calloc((countpaths + 1), sizeof(char *));
 	break_string(PATH, ":", paths);
 	
-	printf("path %s\n", paths[0]);
+
 
 		
 	size = 32;
@@ -118,7 +140,19 @@ int main(int ac, char **av, char **env)
 	
 	if (process_id == 0)
 	{
-		execve(paths[0], ptr, env);
+		if (access(ptr[0], X_OK) == 0)
+		{
+		execve(ptr[0], ptr, env);
+		} else
+		{
+			j = 0;
+			while (paths[j] != NULL)
+			{
+				buffer_path = path[j] + '/' + ptr[0];
+				if (access(buffer, X_OK)
+				++j;
+			}
+		}
 		free(buffer);
 	
 		i = 0;
