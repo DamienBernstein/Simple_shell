6171ffd8bd6fbb2bad004ebd4cd284b49f8729f1
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-11-16 22:25:14 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -44,9 +44,6 @@ void break_string(char *str, char *delimeter, char **ptr, char **env)
 	free(str2);  
 	
 	
-		i = 0;
-	while (ptr[i] != NULL)
-		free(ptr[i]);
 	
 	free(ptr);
 		printf("task complete");
